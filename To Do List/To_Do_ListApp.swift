72cc29235fa88ac2b9d84aff73d16d3d913aa039
Ostap Artym <85745668+artym125@users.noreply.github.com>
2023-01-10 20:21:10 +0200

    Initial Commit

---
+++ To Do List/To_Do_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  To_Do_ListApp.swift
+//  To Do List
+//
+//  Created by Ostap Artym on 10.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct To_Do_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
